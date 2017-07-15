a330e5782491277861c9fbd3160f139359129189
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-07-15 19:04:40 +0530

    Webhook modified and procfile added

--- routes/webhook.js
+++ routes/webhook.js
@@ -3,11 +3,17 @@
  */
 var express = require('express');
 var router = express.Router();
+var config=require("config");
 
 /* GET home page. */
-router.post('/', function(req, res, next) {
-    res.send(req.body.name+" "+"is response");
-    res.sendStatus(200);
+router.get('/', function(req, res, next) {
+    if (req.query['hub.mode'] === 'subscribe' &&
+        req.query['hub.verify_token'] === config.verify_token) {
+        res.status(200).send(req.query['hub.challenge']);
+    } else {
+        console.error("Failed validation. Make sure the validation tokens match.");
+        res.sendStatus(403);
+    }
 });
 
 module.exports = router;
