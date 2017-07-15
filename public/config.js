dbdd5d296f7cda1fcf95534041ba45d540e1b0f9
bharath-koteswarao <v.ramakoteswararao2015@vit.ac.in>
2017-07-15 19:13:19 +0530

    Webhook modified and procfile added

--- public/config.js
+++ public/config.js
@@ -2,7 +2,6 @@
  * Created by koteswarao on 15-07-2017.
  */
 var config={
-    access_token:"",
     verify_token:"message_sent_by_bot",
     access_token:"EAAcMx09IJnEBAKJW3qKYopqWe65lSaYeOpGa0q85JjZCkcXQg1BJG5pIIFy3sTycVinVZBMy7DzZBbDpolZBNzF7nq6vHiiY35RnRKOvKb0Q3PM9O3KfpcnF0haaWlKrUmkO4ODmonbHOziaCaqEiyt1lSz9tAa5AduEAqu1SQZDZD"
 };
